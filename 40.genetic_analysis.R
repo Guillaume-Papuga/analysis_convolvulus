315b5446f9b4426086eb9aaaaf896ac771be1d89
Guillaume-Papuga <guillaume.papuga@gmail.com>
2023-11-02 17:47:31 +0100

    Update genetic data

---
+++ 40.genetic_analysis.R
@@ -0,0 +1,14 @@
+#######################################################
+# Project : Genetic analysis of Convolvulus lanuginosus
+# Script : 40.genetic_analysis
+# Run genetic analysis
+# Authors : Guillaume Papuga
+# Last update : 2 november 2023
+#######################################################
+
+### Project and define the study area
+
+# Plot
+
+# Draw and save the extent
+
